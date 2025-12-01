8dc24d9ae126c32529be180deadb1a756c6170a5
Najeebullah Sadiq <najeebullahsadiq590@gmail.com>
2025-12-01 11:40:32 +0430

    added

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api' // Update with your backend API URL
+  apiUrl: 'http://localhost:5000/api'
 };
--- Frontend/src/environments/environments_prod.ts
+++
@@ -1,5 +0,0 @@
-
-export const environment = {
-  production: true,
-  apiUrl: 'http://103.132.98.92/api' // Update with your production API URL
-};
