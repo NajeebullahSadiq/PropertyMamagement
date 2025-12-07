08e58ad0e5ac4ccd4c79d8fddc48677a47e74ece
Najeebullah Sadiq <najeebullahsadiq590@gmail.com>
2025-12-07 12:12:17 +0430

    Fix: Simplify AddBlockPropertyType migration to only insert Block property type

--- Backend/Migrations/20251207071328_AddBlockPropertyType.cs
+++ Backend/Migrations/20251207071328_AddBlockPropertyType.cs
@@ -10,63 +10,6 @@ namespace WebAPIBackend.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<string>(
-                name: "UserName",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "PhotoPath",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "PhoneNumber",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "LastName",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "FirstName",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Email",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "CompanyName",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text");
-
-            // Insert Block property type
             migrationBuilder.InsertData(
                 table: "PropertyType",
                 schema: "look",
@@ -77,82 +20,11 @@ namespace WebAPIBackend.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // Delete Block property type
             migrationBuilder.DeleteData(
                 table: "PropertyType",
                 schema: "look",
                 keyColumn: "Name",
                 keyValue: "Block");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "UserName",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "PhotoPath",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "PhoneNumber",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "LastName",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "FirstName",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Email",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "CompanyName",
-                table: "UserProfileWithCompany",
-                type: "text",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
         }
     }
 }
