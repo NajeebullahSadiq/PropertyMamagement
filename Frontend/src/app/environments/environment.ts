198220463d20f98611fc8c6952c0f21cc647f456
Najeebullah Sadiq <najeebullahsadiq590@gmail.com>
2025-12-23 14:55:03 +0430

    sdsd

--- Frontend/src/app/environments/environment.ts
+++ Frontend/src/app/environments/environment.ts
@@ -2,10 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  apiURL : 'http://localhost:5143/api'
-};
+export { environment } from '../../environments/environment';
 
 /*
  * For easier debugging in development mode, you can import the following file
